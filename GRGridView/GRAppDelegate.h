4149cde5fb88cbbd72b13e9407270956d30cb2db
Gabriel Rinaldi <gabriel_rinaldi@me.com>
2012-10-19 16:40:21 -0700

    Initial Commit

---
+++ GRGridView/GRAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  GRAppDelegate.h
+//  GRGridView
+//
+//  Created by Gabriel Rinaldi on 10/19/12.
+//  Copyright (c) 2012 Gabriel Rinaldi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GRAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ GRGridView/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  GRGridView
+//
+//  Created by Gabriel Rinaldi on 10/19/12.
+//  Copyright (c) 2012 Gabriel Rinaldi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "GRAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([GRAppDelegate class]));
+    }
+}
